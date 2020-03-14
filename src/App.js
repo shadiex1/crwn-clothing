53b5b647f202df5d43dc913ee930d4a73afdd9d7
shady ahmed <shadiex@hotmail.com>
2020-03-14 16:39:44 +0200

    updating our app with a shop page , collection preview commonent , and colletion component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import HomePage from "./pages/homepage/homepage"
 import './App.css';
-import { Switch,Route} from "react-router-dom"
+import { Switch,Route} from "react-router-dom";
+import Shop from "./pages/shop/shop"
 
 function App() {
   return (
     <div>
       <Switch>
       <Route exact path="/" component={HomePage}/>
+      <Route path= "/shop" component={Shop}/>
       </Switch>
     </div>
   );
