e942fa7ee0eb66aa49cd36b5d87618c83dd15e4c
shady ahmed <shadiex@hotmail.com>
2020-05-05 09:39:42 +0200

    fixed url error

--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./menu-item.scss";
 import {withRouter} from "react-router-dom"
 
-const MenuItem=({title,imageUrl,size,history,linkUrl,match})=>(
-    <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
+const MenuItem=({title,imageUrl,size,history,linkUrl})=>(
+    <div className={`${size} menu-item`} onClick={()=>history.push(`${process.env.PUBLIC_URL + '/shop'}${linkUrl}`)}>
         <div className="background-image" style={{
         backgroundImage:`url(${imageUrl})`
     }} />
